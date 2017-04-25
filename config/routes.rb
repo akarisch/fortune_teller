6f7bbde1f079727218d2d51d21384f3f7038776e
Augustus Karisch <akarisch@users.noreply.github.com>
2017-04-25 13:46:37 -0500

    debugged!
    finished debugging everything, but still getting zero on my grade
    report…

--- config/routes.rb
+++ config/routes.rb
@@ -9,16 +9,16 @@ Rails.application.routes.draw do
 
   # Uncomment each route below ONE AT A TIME and debug.
 
-  # get("/zodiacs/aries",       { :controller =>  zodiacs,  action =>  "ram" })
-  # get("/zodiacs/taurus",      { :controller,   "zodiacs", :action,   "bull" })
-  # get(/zodiacs/gemini,        { :controller => "zodiacs", :action => "twins" })
-  # get("/zodiacs/cancer"       { :controller => "zodiacs"  :action => "crab" })
-  # get ("/zodiacs/leo",        { :controller => "zodiac",  :action => "lion" })
-  # get(" /zodiacs/virgo",      { :controller => "Zodiacs", :action => "maiden" })
-  # get("/zodiacs/libra",       { :controller => "zodiacs", :action => "scales" })
-  # get("/zodiac/scorpio",      { :controller => "zodiacs", :action => "scorpion" })
-  # get("/zodiacs/sagittarius", { :controller => "zodiacs", :action => "archer" })
-  # get("/zodiacs/capricorn",   { :controller => "zodiacs", :action => "goat" })
-  # get("/zodiacs/aquarius",    { :controller => "zodiacs", :action => "waterbearer" })
-  # get("/zodiacs/pisces",      { :controller => "zodiacs", :action => "fish" })
+  get("/zodiac/aries",       {:controller =>  "zodiac",  :action =>  "ram" })
+  get("/zodiac/taurus",      { :controller =>   "zodiac", :action =>   "bull" })
+  get("/zodiac/gemini",        { :controller => "zodiac", :action => "twins" })
+  get("/zodiac/cancer",       { :controller => "zodiac",  :action => "crab" })
+  get("/zodiac/leo",        { :controller => "zodiac",  :action => "lion" })
+  get("/zodiac/virgo",      { :controller => "zodiac", :action => "maiden" })
+  get("/zodiac/libra",       { :controller => "zodiac", :action => "scales" })
+  get("/zodiac/scorpio",      { :controller => "zodiac", :action => "scorpion" })
+  get("/zodiac/sagittarius", { :controller => "zodiac", :action => "archer" })
+  get("/zodiac/capricorn",   { :controller => "zodiac", :action => "goat" })
+  get("/zodiac/aquarius",    { :controller => "zodiac", :action => "waterbearer" })
+  get("/zodiac/pisces",      { :controller => "zodiac", :action => "fish" })
 end
